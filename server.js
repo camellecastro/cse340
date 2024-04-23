6085897f9ef2471c433137ef0a1743bf0530f0ce
camellecastro <camelleccastro@gmail.com>
2024-04-23 10:26:33 -0600

    Learning Activity

--- server.js
+++ server.js
@@ -6,14 +6,27 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") //not a view
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+//Index route
+app.get("/", function (req, res) {
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
