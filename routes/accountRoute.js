6fd83479b05a2e50eb24763d4a2a2d49e6365314
camellecastro <camelleccastro@gmail.com>
2024-05-15 19:22:49 -0600

    Learning Activities

---
+++ routes/accountRoute.js
@@ -0,0 +1,46 @@
+// Needed Resources
+const express = require("express");
+const router = new express.Router();
+const accountController = require("../controllers/accountController");
+const utilities = require("../utilities");
+const regValidate = require("../utilities/account-validation");
+
+/* ***********************
+ * Deliver Log in View
+ *************************/
+router.get(
+  "/login",
+  utilities.handleErrors(accountController.buildLogin)
+);
+
+/* ***********************
+ * Deliver Registration View
+ *************************/
+router.get(
+  "/register",
+  utilities.handleErrors(accountController.buildRegister)
+);
+
+/* ***********************
+ * Enable/Create Registration
+ *************************/
+router.post(
+  "/register",
+  regValidate.registrationRules(),
+  regValidate.checkRegData,
+  utilities.handleErrors(accountController.registerAccount)
+);
+
+/* ***********************
+ * Process the log in attempt
+ *************************/
+router.post(
+  "/login",
+  (req, res) => {
+    res.status(200).send('login process')
+  }
+)
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,15 +5,46 @@
 /* ***********************
  * Require Statements
  *************************/
-const express = require("express")
-const expressLayouts = require("express-ejs-layouts")
-const env = require("dotenv").config()
-const app = express()
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+const env = require("dotenv").config();
+const app = express();
+const bodyParser = require("body-parser");
+const utilities = require("./utilities/");
+
+const session = require("express-session");
+const pool = require("./database/");
+
 const baseController = require("./controllers/baseController")
+
 const static = require("./routes/static");
 const inventoryRoute = require("./routes/inventoryRoute")
 const testRoute = require("./routes/testRoute")
-const utilities = require("./utilities/")
+const accRoute = require("./routes/accountRoute")
+
+/* ***********************
+ * Middleware
+ * ************************/
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+ }))
+
+ // Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 
 /* ***********************
@@ -33,6 +64,8 @@ app.get("/", utilities.handleErrors(baseController.buildHome))
 app.use("/inv", inventoryRoute)
 // Test Route
 app.use("/trigger-error", testRoute)
+// Account Route 
+app.use("/account", accRoute)
 
 /* ***********************
 * Middleware
