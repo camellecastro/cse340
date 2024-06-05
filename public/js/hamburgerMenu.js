78af01385ad30eef289373b238946056d8a60de0
camellecastro <camelleccastro@gmail.com>
2024-06-04 22:52:55 -0600

    Finalization

---
+++ public/js/hamburgerMenu.js
@@ -0,0 +1,12 @@
+// Store the selected elements that we are going to use. 
+const mainnav = document.querySelector('.nav')
+const hambutton = document.querySelector('#menu');
+
+// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
+
+
+
