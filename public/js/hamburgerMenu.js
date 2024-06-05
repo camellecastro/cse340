f9dec60041ba190e4b21064936f032df3bdc5ec7
camellecastro <camelleccastro@gmail.com>
2024-06-04 23:59:20 -0600

    Week 6 Finalized Project

--- public/js/hamburgerMenu.js
+++ public/js/hamburgerMenu.js
@@ -1,12 +1,7 @@
-// Store the selected elements that we are going to use. 
-const mainnav = document.querySelector('.nav')
-const hambutton = document.querySelector('#menu');
+const hamburger = document.querySelector("#menu");
+const nav = document.querySelector(".nav");
 
-// Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
-hambutton.addEventListener('click', () => {
-	mainnav.classList.toggle('show');
-	hambutton.classList.toggle('show');
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("show");
+  hamburger.classList.toggle("show");
 });
-
-
-
